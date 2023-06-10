0d1ad2a253f5988b1575fd2aadc7cf2ab89b538a
Joshua Coleman <jlcolema@icloud.com>
2023-06-09 19:43:22 -0500

    Initial commit.

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Joshua Coleman",
+	url: "https://joshuacoleman.dev/",
+	language: "en",
+	description: "The personal website of Joshua Coleman.",
+	author: {
+		name: "Joshua Coleman",
+		email: "hello@joshuacoleman.dev",
+		url: "https://joshuacoleman.dev/about/"
+	}
+}
